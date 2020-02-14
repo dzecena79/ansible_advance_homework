f72528d67a5a9e627391d8449ea953f1194eaf5a
root <root@bastion.ff4f.example.opentlc.com>
2020-02-14 00:04:09 +0000

    FIRST CHANGES TO ERROS IN THE INITIAL PLAYBOOCKS PROBLEM WITH TOWER LICENSING EXPIRED WAITING FOR A NEW LICENSE

---
+++ set-environment-values.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+OSP_GUID="9e52"
+TOWER_GUID="ff4f"
+OPENTLC_LOGIN="daniel.zecenaturcios-atos.net"
+GITHUB_REPO="https://github.com/dzecena79/ansible_advance_homework"
+JQ_REPO_BASE="http://www.opentlc.com/download/ansible_bootcamp"
+REGION="us-east-1"
+RH_MAIL_ID="daniel.zecenaturcios@atos.net"
